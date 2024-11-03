db38e44ed2f991ad30998b049c99e3375b1754a6
Manuel Bogner <manuel.bogner@openresearch.com>
2024-11-03 22:46:56 +0100

    release v1.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,13 +14,13 @@ dependencyManagement {
     dependencies {
         dependency("dev.mbo:kotlin-logging:1.1.0")
         dependency("dev.mbo:spring-kotlin-cache:1.1.0")
+        dependency("dev.mbo:spring-kotlin-reflection:1.1.0")
         dependency("dev.mbo:spring-kotlin-error:1.1.0")
         dependency("dev.mbo:spring-kotlin-jpa:1.2.0")
-        dependency("dev.mbo:spring-kotlin-reflection:1.1.0")
         dependency("dev.mbo:spring-kotlin-s3:1.1.0")
+        dependency("dev.mbo:spring-kotlin-validation:1.1.0")
         dependency("dev.mbo:spring-kotlin-smtp:1.1.0")
         dependency("dev.mbo:spring-kotlin-templating:1.1.0")
-        dependency("dev.mbo:spring-kotlin-validation:1.1.0")
         dependency("dev.mbo:spring-kotlin-web:1.1.0")
     }
 }
