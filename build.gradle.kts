dfae1fafa4e0db040f765fe036c1cb17ef7d419f
Manuel Bogner <manuel.bogner@openresearch.com>
2024-12-15 15:21:47 +0100

    prepare release

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,16 +14,16 @@ group = "dev.mbo"
 
 dependencyManagement {
     dependencies {
-        dependency("dev.mbo:kotlin-logging:1.1.6")
-        dependency("dev.mbo:spring-kotlin-cache:1.1.1")
-        dependency("dev.mbo:spring-kotlin-reflection:1.1.1")
-        dependency("dev.mbo:spring-kotlin-error:1.1.1")
-        dependency("dev.mbo:spring-kotlin-jpa:1.2.1")
-        dependency("dev.mbo:spring-kotlin-s3:1.1.1")
-        dependency("dev.mbo:spring-kotlin-validation:1.1.1")
-        dependency("dev.mbo:spring-kotlin-smtp:1.1.1")
-        dependency("dev.mbo:spring-kotlin-templating:1.1.1")
-        dependency("dev.mbo:spring-kotlin-web:1.1.1")
+        dependency("dev.mbo:kotlin-logging:1.1.7")
+        dependency("dev.mbo:spring-kotlin-cache:1.1.2")
+        dependency("dev.mbo:spring-kotlin-reflection:1.1.2")
+        dependency("dev.mbo:spring-kotlin-error:1.1.2")
+        dependency("dev.mbo:spring-kotlin-jpa:1.2.2")
+        dependency("dev.mbo:spring-kotlin-s3:1.1.2")
+        dependency("dev.mbo:spring-kotlin-validation:1.1.2")
+        dependency("dev.mbo:spring-kotlin-smtp:1.1.2")
+        dependency("dev.mbo:spring-kotlin-templating:1.1.2")
+        dependency("dev.mbo:spring-kotlin-web:1.1.2")
     }
 }
 
