99f03d719d79846d3ec0411375ebbfebdbc318e3
Manuel Bogner <manuel.bogner@openresearch.com>
2025-02-09 23:05:05 +0100

    prepare release

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,16 +14,16 @@ group = "dev.mbo"
 
 dependencyManagement {
     dependencies {
-        dependency("dev.mbo:kotlin-logging:1.1.9")
-        dependency("dev.mbo:spring-kotlin-cache:1.1.4")
-        dependency("dev.mbo:spring-kotlin-reflection:1.1.4")
-        dependency("dev.mbo:spring-kotlin-error:1.1.4")
-        dependency("dev.mbo:spring-kotlin-jpa:1.2.4")
-        dependency("dev.mbo:spring-kotlin-s3:1.1.4")
-        dependency("dev.mbo:spring-kotlin-validation:1.1.4")
-        dependency("dev.mbo:spring-kotlin-smtp:1.1.4")
-        dependency("dev.mbo:spring-kotlin-templating:1.1.4")
-        dependency("dev.mbo:spring-kotlin-web:1.1.4")
+        dependency("dev.mbo:kotlin-logging:1.1.10")
+        dependency("dev.mbo:spring-kotlin-cache:1.1.5")
+        dependency("dev.mbo:spring-kotlin-reflection:1.1.5")
+        dependency("dev.mbo:spring-kotlin-error:1.1.5")
+        dependency("dev.mbo:spring-kotlin-jpa:1.2.5")
+        dependency("dev.mbo:spring-kotlin-s3:1.1.5")
+        dependency("dev.mbo:spring-kotlin-validation:1.1.5")
+        dependency("dev.mbo:spring-kotlin-smtp:1.1.5")
+        dependency("dev.mbo:spring-kotlin-templating:1.1.5")
+        dependency("dev.mbo:spring-kotlin-web:1.1.5")
     }
 }
 
@@ -116,6 +116,6 @@ signing {
 
 tasks.wrapper {
     // https://gradle.org/releases/
-    gradleVersion = "8.12"
+    gradleVersion = "8.12.1"
     distributionType = Wrapper.DistributionType.BIN
 }
